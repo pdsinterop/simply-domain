bc914e699f60aa88207d52b2f66dfb558b7b7465
aKacharava <a.kacharava94@gmail.com>
2022-09-13 13:51:16 +0200

    update dependencies and update error handling

--- proxy-servers/dns-proxy-server/index.js
+++ proxy-servers/dns-proxy-server/index.js
@@ -1,11 +1,18 @@
-let dns = require('native-dns');
+let dns = require("native-dns");
 let server = dns.createServer();
 
-server.on('listening', function(){
-  console.log(`Server listening on ${server.address()}`);
-})
-server.on('error', function(err, buff, req, res){
+server.on("listening", function(){
+    console.log("server listening on", server.address())
+  }
+);
+server.on("close", function(){
+  console.log("server closed", server.address());
+});
+server.on("error", function(err, buff, req, res){
   console.error(err.stack);
-})
+});
+server.on("socketError", function(err, socket){
+  console.error(err);
+});
 
-server.serve(53)
+server.serve(53);
