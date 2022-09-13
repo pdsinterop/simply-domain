25286fd71b10e92e4d9b3fb4258017a6decd4353
Alexander Kacharava <alexander@muze.nl>
2022-09-13 12:39:51 +0200

    Create basic dns-proxy-server

---
+++ proxy-servers/dns-proxy-server/index.js
@@ -0,0 +1,11 @@
+let dns = require('native-dns');
+let server = dns.createServer();
+
+server.on('listening', function(){
+  console.log(`Server listening on ${server.address()}`);
+})
+server.on('error', function(err, buff, req, res){
+  console.error(err.stack);
+})
+
+server.serve(53)
